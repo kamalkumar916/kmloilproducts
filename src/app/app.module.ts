d4328cbe318a5c13e49375c07c6610f591132749
kamalkumar916 <kamalkumar91613@gmail.com>
2020-02-18 22:38:50 +0530

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HelloComponent } from './hello.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import {InputTextModule} from 'primeng/inputtext';
+import { RegisterComponent } from './register/register.component';
+import { OilProductDetailsComponent } from './oil-product-details/oil-product-details.component';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
@@ -18,7 +20,7 @@ const appRoutes: Routes = [
       appRoutes,
       { enableTracing: true } 
     ) ],
-  declarations: [ AppComponent, HelloComponent, LoginComponent ],
+  declarations: [ AppComponent, HelloComponent, LoginComponent, RegisterComponent, OilProductDetailsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/oil-product-details/oil-product-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oil-product-details',
+  templateUrl: './oil-product-details.component.html',
+  styleUrls: ['./oil-product-details.component.css']
+})
+export class OilProductDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
