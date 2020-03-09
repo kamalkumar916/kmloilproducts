1ab025540acde7c80a1f8cbd0adcc164ba9a304d
kamalkumar916 <kamalkumar91613@gmail.com>
2020-03-09 12:26:03 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+   providers: [{provide: APP_BASE_HREF, useValue : '/' }]
 })
 export class AppComponent  {
 showSignup:boolean=false;
